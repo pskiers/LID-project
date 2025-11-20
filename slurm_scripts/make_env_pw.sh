87c049e734d60b3c72e8faeceed73e3a87071329
Pawel Skiers <pawelsk269@gmail.com>
2025-11-20 14:22:06 +0100

    Update diffusion_memorization

---
+++ slurm_scripts/make_env_pw.sh
@@ -0,0 +1,10 @@
+python3 -m venv env/
+source env/bin/activate
+
+pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu124
+
+pip install --no-cache -r helios_requirements.txt
+cd diffusers/examples/dreambooth
+pip install --no-cache -r requirements.txt
+pip install --no-cache -r requirements_sdxl.txt
+pip install --no-cache -r requirements_flax.txt
