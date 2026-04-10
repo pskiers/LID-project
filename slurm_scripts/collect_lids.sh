481e4dace2d46db85558003ac38c8a525e3eb8e3
Pawel Skiers <pawelsk269@gmail.com>
2026-04-10 22:39:30 +0200

    Clean up the repo

--- slurm_scripts/collect_lids.sh
+++ slurm_scripts/collect_lids.sh
@@ -111,25 +111,244 @@ export SSL_CERT_FILE=/net/home/plgrid/plgpawel269/cacert.pem
 #   --num_samples 10000 \
 #   --max_bs 250
 
-python -m src.scripts.train \
-  --dataset_path /home/pskiers/LID-project/diffusion_memorization/outputs/sdxl-toy-a-lot/grads/t50 \
-  --hookpoints doesnotmatter \
-  --lr 0.00003 \
-  --lr_warmup_steps 1000 \
-  --wandb_project "sae_text_token_grads" \
-  --expansion_factor 8 \
-  --k 32 \
-  --batch_topk true \
-  --num_epochs 1000 \
-  --save_every 1000 \
-  --run_name toy-k32-exp-factor-8 \
-  --effective_batch_size 512
+# python -m src.scripts.train \
+#   --dataset_path /home/pskiers/LID-project/diffusion_memorization/outputs/sdxl-toy-a-lot/grads/t50 \
+#   --hookpoints doesnotmatter \
+#   --lr 0.00003 \
+#   --lr_warmup_steps 1000 \
+#   --wandb_project "sae_text_token_grads" \
+#   --expansion_factor 8 \
+#   --k 32 \
+#   --batch_topk true \
+#   --num_epochs 1000 \
+#   --save_every 1000 \
+#   --run_name toy-k32-exp-factor-8 \
+#   --effective_batch_size 512
+
+# cd ../diffusion_memorization/
+# python compare_subspaces.py \
+#     --dirs sae:../universal-diffsae/sae-ckpts/sae_text_token_grads/toy-new-32-exp-factor-8-t38_t38 \
+#     --prompt="A picture of a toy" \
+#     --token="toy" \
+#     --sae_low=-6 \
+#     --sae_high=0 \
+#     --output_dir=outputs/intreventions/toy_concept_sae_k32_exp_factor8_38
+
+# python -m src.scripts.train \
+#   --dataset_path /home/pskiers/LID-project/diffusion_memorization/outputs/sdxl-toy-new/grads/t38/shards \
+#   --hookpoints doesnotmatter \
+#   --lr 0.00003 \
+#   --lr_warmup_steps 1000 \
+#   --wandb_project "sae_text_token_grads" \
+#   --expansion_factor 8 \
+#   --k 32 \
+#   --batch_topk true \
+#   --num_epochs 20 \
+#   --save_every 1000 \
+#   --run_name toy-new-32-exp-factor-8-t38 \
+#   --effective_batch_size 512
 
 
-python compare_subspaces.py \
-    --dirs sae:../universal-diffsae/sae-ckpts/monster-k8-exp-factor-8_grads \
-    --prompt="A picture of a monster" \
-    --token="monster" \
-    --sae_low=-6 \
-    --sae_high=0 \
-    --output_dir=outputs/intreventions/monster_concept_sae_k8_exp_factor8_0-1.5
+# python token_subspace.py \
+#     name=sdxl-turbo-monster-2 \
+#     model.model_id=stabilityai/sdxl-turbo \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0 \
+#     data.prompt="A picture of a monster" \
+#     data.token="monster" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_monster \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+
+# python token_subspace.py \
+#     name=sdxl-dmd-car \
+#     model.model_id=sdxl-dmd \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0.0 \
+#     data.prompt="A picture of a car" \
+#     data.token="car" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_car \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python token_subspace.py \
+#     name=sdxl-dmd-monster \
+#     model.model_id=sdxl-dmd \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0.0 \
+#     data.prompt="A picture of a monster" \
+#     data.token="monster" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_monster \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python token_subspace.py \
+#     name=sdxl-dmd-dog \
+#     model.model_id=sdxl-dmd \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0 \
+#     data.prompt="A picture of a dog" \
+#     data.token="dog" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_dog \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python token_subspace.py \
+#     name=sdxl-dmd-cat \
+#     model.model_id=sdxl-dmd \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0 \
+#     data.prompt="A picture of a cat" \
+#     data.token="cat" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_cat \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python token_subspace.py \
+#     name=sdxl-dmd-person \
+#     model.model_id=sdxl-dmd \
+#     model.num_inference_steps=4 \
+#     model.guidance_scale=0 \
+#     data.prompt="A picture of a person" \
+#     data.token="person" \
+#     data.timesteps=[4] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_dmd_person \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python token_subspace.py \
+#     name=sdxl-cat \
+#     model.num_inference_steps=50 \
+#     model.guidance_scale=7.5 \
+#     data.prompt="A picture of a cat" \
+#     data.token="cat" \
+#     data.timesteps=[50] \
+#     data.image_folder=/home/pskiers/LID-project/data/sdxl_cat \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true
+
+# python -m src.scripts.train \
+#   --dataset_path /home/pskiers/LID-project/diffusion_memorization/outputs/sdxl-dmd-person/grads/t4/shards \
+#   --hookpoints doesnotmatter \
+#   --lr 0.00003 \
+#   --lr_warmup_steps 1000 \
+#   --wandb_project "sae_text_token_grads" \
+#   --expansion_factor 1 \
+#   --k 32 \
+#   --batch_topk false \
+#   --num_epochs 20 \
+#   --save_every 1000 \
+#   --run_name sdxl-dmd-person-k32-exp-factor-1-notopk \
+#   --effective_batch_size 512 \
+#   --seed 31
+
+
+
+# python compare_subspaces.py \
+#     --dirs sae:../universal-diffsae/sae-ckpts/sae_text_token_grads/sdxl-dmd-person-k32-exp-factor-1-notopk_t4 \
+#     --prompt="A picture of a person" \
+#     --token="person" \
+#     --sae_low=-6 \
+#     --sae_high=0 \
+#     --output_dir=outputs/intreventions/sdxl-dmd_person_sae_k32_exp_factor1_notopk_0-6-random-3 \
+#     --random_n=3 \
+#     --model="sdxl-dmd" \
+#     --num_samples=8 \
+#     --batch_size=4 \
+#     --intervention_strengths 0 1 5 10 30 50 70 100 150 200 \
+#     --num_interventions=8
+
+# python compare_subspaces.py \
+#     --dirs grad:outputs/sdxl-dmd-person/grads/t4/shards \
+#     --prompt="A picture of a person" \
+#     --token="person" \
+#     --sae_low=-6 \
+#     --sae_high=0 \
+#     --output_dir=outputs/intreventions/sdxl-dmd-person-pca-3-rand \
+#     --random_n=3 \
+#     --model="sdxl-dmd" \
+#     --num_samples=8 \
+#     --batch_size=4 \
+#     --intervention_strengths 0 1 5 10 30 50 70 100 150 200 \
+#     --num_interventions=8
+
+# python compare_subspaces.py \
+#     --dirs grad:outputs/sdxl-dmd-cat/grads/t4/shards \
+#     --prompt="A picture of a cat" \
+#     --token="cat" \
+#     --sae_low=-6 \
+#     --sae_high=0 \
+#     --output_dir=outputs/intreventions/sdxl-cat-random \
+#     --random_n=1 \
+#     --model="sdxl" \
+#     --num_samples=8 \
+#     --batch_size=4 \
+#     --intervention_strengths 0 30 50 70 100 \
+#     --num_interventions=2
+
+
+
+# python sample_with_directions.py \
+#   name=sdxl-dmd-person-samples-with-directions \
+#   prompt="A picture of a person" \
+#   token="person" \
+#   min_intervention_strenght=50 \
+#   max_intervention_strenght= \
+#   batch_size=4 \
+#   directions_path="../universal-diffsae/sae-ckpts/sae_text_token_grads/sdxl-dmd-person-k32-exp-factor-1-notopk_t4" \
+#   direction_type="sae"
+
+# python sample_with_directions.py \
+#   name=sdxl-dmd-person-samples-with-directions \
+#   prompt="A picture of a person" \
+#   token="person" \
+#   min_intervention_strenght=30 \
+#   max_intervention_strenght=40 \
+#   batch_size=4 \
+#   directions_path="outputs/sdxl-dmd-person/grads/t4/shards" \
+#   direction_type="grad"
+
+
+
+
+# python token_subspace.py \
+#     name=sdxl-an-image-of-a-monster \
+#     model.num_inference_steps=50 \
+#     model.guidance_scale=7.5 \
+#     data.prompt="An image of a monster" \
+#     data.token="monster" \
+#     data.timesteps=[50] \
+#     data.image_folder=/data/pskiers/concept_dimention/sdxl_monster \
+#     remove_common=false \
+#     remove_significant_directions=false \
+#     randomize_pairs=true \
+#     max_pairs=10000 \
+#     shard_size=500
+
+
+python token_subspace.py \
+    name=sdxl-just-monster \
+    model.num_inference_steps=50 \
+    model.guidance_scale=7.5 \
+    data.prompt="monster" \
+    data.token="monster" \
+    data.timesteps=[50] \
+    data.image_folder=/data/pskiers/concept_dimention/sdxl_monster \
+    remove_common=false \
+    remove_significant_directions=false \
+    randomize_pairs=true \
+    max_pairs=10000 \
+    shard_size=500
